52079bd1ab71618b20563048d5c62b66c4718269
Terdsak Boonsak <guide.band8@gmail.com>
2023-06-19 16:03:55 +0700

    Learn Routing express
    add ejs multi-webpage

--- nodejs/app.js
+++ nodejs/app.js
@@ -3,11 +3,14 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const app = express();
-const port = process.env.port;
+const port = process.env.port || 4000;
 const path = require('path');
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, "/public/")));
+
+app.get("views", "./src/views");
+app.set("view engine", "ejs");
 
 app.get("/", (req,res) =>{
     res.send('file not found');
