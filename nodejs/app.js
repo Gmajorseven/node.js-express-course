4f223f874d80336859e33bba06fe114376521671
Terdsak Boonsak <guide.band8@gmail.com>
2023-06-23 21:07:34 +0700

    update

--- nodejs/app.js
+++ nodejs/app.js
@@ -3,19 +3,28 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const app = express();
+const detailRouter = express.Router();
+
 const port = process.env.port || 4000;
 const path = require('path');
 
-app.use(morgan('combined'));
-app.use(express.static(path.join(__dirname, "/public/")));
 
-app.get("views", "./src/views");
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, "/src/views/public")));
+
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-app.get("/", (req,res) =>{
-    res.send('file not found');
-})
+detailRouter.route("/detail").get((req, res) => {
+    res.render('detail');
+});
 
-app.listen(port, ()=>{
+app.use("/shop/", detailRouter)
+
+app.get("/shop", (req, res) => {
+    res.render('shop', { username: 'Teddy' });
+});
+
+app.listen(port, () => {
     debug("Listening on Port : " + chalk.green(port));
-})
+});
